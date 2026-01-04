b486e86cf3780689d7edeba436fe7a96aa342bfa
mohsen <amirimohsen72@gmail.com>
2026-01-04 22:37:43 +0330

    #6 account and customer model

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,28 @@ class CustomUser(AbstractUser):
         verbose_name_plural = _('accounts')
 
     age = models.PositiveIntegerField(null=True,blank=True,verbose_name=_('age')) #adad mosbat : sen
-    mobile = models.CharField(max_length=11,blank=True,verbose_name=_('mobile')) #shomare mobile
+    phone_number = models.CharField(max_length=15, null=True, blank=True, verbose_name=_('phone number'))
 
+    def __str__(self):
+        if self.get_full_name():
+            return self.get_full_name()
+        else:
+            return self.username
+
+
+
+class Customer(models.Model):
+    class Meta:
+        verbose_name = _('customer')
+        verbose_name_plural = _('customers')
+        ordering = ['-created_at']
+    first_name = models.CharField(max_length=100 , verbose_name=_('first name'))
+    last_name = models.CharField(max_length=100 , verbose_name=_('last name'))
+    phone_number = models.CharField(max_length=15, null=True, blank=True, verbose_name=_('phone number'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created at') )
+    is_active = models.BooleanField(default=True , verbose_name=_('active'))
+    note = models.TextField(blank=True, verbose_name=_('note'))
+
+    def __str__(self):
+        return f"{ self.first_name} {self.last_name} ({self.phone_number})"    
 
